09cefbbbfddaac8fe57d491a856f630df1d6d8e3
gromov-nikita <hromay.n999@gmail.com>
2021-04-11 13:05:17 +0300

    1

---
+++ 2)/src/MyClass.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyClass<T> {
+    private T obj1;
+    private T obj2;
+    private T obj3;
+    public MyClass(T arg1, T arg2, T arg3) {
+        obj1 = arg1;
+        obj2 = arg2;
+        obj3 = arg3;
+    }
+    public void setObj1(T obj1) {
+        this.obj1 = obj1;
+    }
+    public void setObj2(T obj2) {
+        this.obj2 = obj2;
+    }
+    public void setObj3(T obj3) {
+        this.obj3 = obj3;
+    }
+    public T getObj1() {
+        return obj1;
+    }
+    public T getObj2() {
+        return obj2;
+    }
+    public T getObj3() {
+        return obj3;
+    }
+}
---
+++ 2)/src/Runner.java
@@ -0,0 +1,7 @@
+/*
+Exercise 2: (1) Create a holder class that holds three objects of the same type, along with
+the methods to store and fetch those objects and a constructor to initialize all three.
+ */
+public class Runner {
+
+}
